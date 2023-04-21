96980c17b2faa917b8647431ed0d6fc9e26ef278
Teresa Moser <moserte@byui.edu>
2023-04-20 20:41:37 -0600

    Update index.js

--- lesson01/index.js
+++ lesson01/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send('Marlayna Eliza Moser')
+    res.send(<h2>'Marlayna Eliza Moser'</h2>)
 })
 
 app.listen(3000, () => {
