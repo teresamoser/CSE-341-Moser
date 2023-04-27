376f3889d6fc7fb8152846b1c732fa336d70e4e5
Teresa Moser <moserte@byui.edu>
2023-04-26 19:57:08 -0600

    hide code

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongodb = require('./db/connect');
 
-const port = process.env.PORT || 8080;
-const app = express();
+// const port = process.env.PORT || 8080;
+// const app = express();
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
+// app
+//   .use(bodyParser.json())
+//   .use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     next();
+//   })
+//   .use('/', require('./routes'));
 
-mongodb.initDb((err, _mongodb) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
-});
+// mongodb.initDb((err, _mongodb) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     app.listen(port);
+//     console.log(`Connected to DB and listening on ${port}`);
+//   }
+// });
