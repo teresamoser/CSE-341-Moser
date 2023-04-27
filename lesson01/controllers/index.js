8bc59677b1f4f4899e329dee07a1c554604fa4da
Teresa Moser <moserte@byui.edu>
2023-04-26 19:33:45 -0600

    Lesson01 commit final

---
+++ lesson01/controllers/index.js
@@ -0,0 +1,5 @@
+const nameFunction = (req, res, next) => {
+    res.json('Marlayna Eliza Moser');
+}
+
+module.exports = { nameFunction };
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
-app.use('/', require('./routes'))
+app.use('/', require('./lesson01/routes'))
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}`)
+app.listen(3000, () =>{
+    console.log(`Server is running on ${port}`);
 })
