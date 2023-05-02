f352dc948274cfecf75e5acb8c40f9740e105d45
Teresa Moser <moserte@byui.edu>
2023-05-01 22:32:19 -0600

    fixed 8080 error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongodb = require('./db/connect');
+const mongodb = require('./lesson02/db/connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
