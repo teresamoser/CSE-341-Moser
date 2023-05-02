f83e338d02c1fb1f6171b935c3abd04e710e81e1
Teresa Moser <moserte@byui.edu>
2023-05-01 22:24:45 -0600

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
-const port = process.env.port || 3000;
+// const express = require('express');
+// const app = express();
+// const port = process.env.port || 3000;
 
-app.use('/', require('./lesson02/routes'))
+// app.use('/', require('./lesson02/routes'))
 
-app.listen(3000, () =>{
-    console.log(`Server is running on ${port}`);
-})
+// app.listen(3000, () =>{
+//     console.log(`Server is running on ${port}`);
+// })
