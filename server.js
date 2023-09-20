6a8a452e67633425152539f0127eeb9ef45b4349
Teresa Moser <moserte@byui.edu>
2023-09-19 20:07:02 -0600

    save removing lesson02

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const port = process.env.port || 3000;
-
-// app.use('/', require('./lesson02/routes'))
-
-// app.listen(3000, () =>{
-//     console.log(`Server is running on ${port}`);
-// })
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
