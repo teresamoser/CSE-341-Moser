42621883bbf59f16ff6c44282e5c6654abdb8bec
Teresa Moser <moserte@byui.edu>
2023-09-23 22:34:55 -0600

    save updates

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
-app.use('/', require('./routes'))
+app.use('/', require('./lesson02/routes'))
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`)
