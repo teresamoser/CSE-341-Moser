825e339f92329df53e08edb3f33887d8e449d4a6
Teresa Moser <moserte@byui.edu>
2023-09-23 23:11:30 -0600

    save fixes

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongodb = require('./lesson02/db/connect.js');
+const mongodb = require('./lesson02/db/connect');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app
