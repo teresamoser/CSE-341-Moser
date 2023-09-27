8269a64540da3f7e58943b5fa7c397ad34a1183c
Teresa Moser <moserte@byui.edu>
2023-09-26 23:12:56 -0600

    save updates

--- lesson02/controllers/contacts.js
+++ lesson02/controllers/contacts.js
@@ -3,7 +3,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('Contacts').find();
+  const result = await mongodb.getDb().db().collection('Contacts.PeopleIknow').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
@@ -15,7 +15,7 @@ const getSingle = async (req, res, next) => {
   const result = await mongodb
     .getDb()
     .db()
-    .collection('Contacts')
+    .collection('Contacts.PeopleIknow')
     .find({ _id: userId });
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
