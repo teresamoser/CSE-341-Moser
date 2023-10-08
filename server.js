c44be4982a0cda50a01dd71ded39852ef58d2251
Teresa Moser <moserte@byui.edu>
2023-10-07 21:06:30 -0600

    save changes

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const cors = require ('cors');
+const app = express();
 const bodyParser = require('body-parser');
 const mongodb = require('./lesson02/db/connect');
 
-const port = process.env.PORT || 8080;
-const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app
+  .use('/api-docs', swaggerUi.service, swaggerUi.setup(swaggerDocument))
+  .use(cors())
+  .use(express.json())
+  .use(express.urlencoded({extended: true}))
+  .use('./', require('./lesson02/routes'))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,11 +20,31 @@ app
   })
   .use('/', require('./lesson02/routes'));
 
-mongodb.initDb((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on ${port}`);
-  }
-});
+const db = require('./models');
+  db.mongoose
+  .connect(db.url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to the Database!');
+  })
+  .catch((err) => {
+    console.log('Cannot connect to the Database!', err);
+    process.exit();
+  });
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}.`)
+  });
+
+  mongodb.initDb((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to DB and listening on ${port}`);
+    }
+  });
+  
